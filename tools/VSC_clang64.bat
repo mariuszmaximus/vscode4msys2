8cb142c943936e917990da02015461eef77cb89e
mariuszmaximus <mariusz.maximus@gmail.com>
2023-01-18 20:38:31 +0100

    first version

---
+++ tools/VSC_clang64.bat
@@ -0,0 +1,12 @@
+cd /d %~dp0
+cd ..
+
+set VSCODE_EXE="C:\Users\%USERNAME%\AppData\Local\Programs\Microsoft VS Code\code.exe"
+set MSYS2_ROOT=C:\msys64
+
+@rem ustawiam środowisko 
+@rem   czysty PATH bez ustawien komputera 
+@rem   tylko VSC + kompilator + ninja + cmake
+set PATH=C:\Windows\System32;%MSYS2_ROOT%\usr\bin;%MSYS2_ROOT%\clang64\bin
+
+%VSCODE_EXE% .\ 
