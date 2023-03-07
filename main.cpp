2b214039df42c17d79809f409d9982c7b3056e8e
max <max@draminski.com>
2023-03-07 13:48:26 +0100

    przyklad boost

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " ");
 }
